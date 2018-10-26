b322812d4853cf9494642f29a49310a37cc6650b
Ioan Istrate <ioan1@Ioans-MBP.frontierlocal.net>
2018-10-26 14:17:04 -0400

    adds program entry

---
+++ main.cpp
@@ -0,0 +1,17 @@
+//
+//  main.cpp
+//  warcraft
+//
+//  Created by Ioan Istrate on 10/26/18.
+//  Copyright © 2018 Ioan Istrate. All rights reserved.
+//
+
+#include "Game.h"
+
+int main(int argc, char** args) {
+    
+    Game game;
+    game.run();
+    
+    return 0;
+}
