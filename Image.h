681eb6834de38cc07e9036a1b3e428170d3e2edb
Ioan Istrate <ioan1@Ioans-MBP.frontierlocal.net>
2018-10-26 14:17:58 -0400

    adds image class

---
+++ Image.h
@@ -0,0 +1,16 @@
+//
+//  Image.h
+//  warcraft
+//
+//  Created by Ioan Istrate on 10/26/18.
+//  Copyright © 2018 Ioan Istrate. All rights reserved.
+//
+
+#ifndef IMAGE_H
+#define IMAGE_H
+
+class Image {
+    
+};
+
+#endif
