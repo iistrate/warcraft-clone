fe182cbf9a78cd30544c76233a70855ba656dfde
Ioan Istrate <ioan1@Ioans-MBP.frontierlocal.net>
2018-10-26 14:16:43 -0400

    adds game class

---
+++ Game.h
@@ -0,0 +1,24 @@
+//
+//  Game.h
+//  warcraft
+//
+//  Created by Ioan Istrate on 10/26/18.
+//  Copyright © 2018 Ioan Istrate. All rights reserved.
+//
+
+#ifndef GAME_H
+#define GAME_H
+
+#include <iostream>
+
+#include "globals.h"
+
+class Game {
+public:
+    void run() {
+        
+        std::cout << "Game loop initiated\n";
+    }
+};
+
+#endif
