70e4386c44ca87b0ff372971d7314c699f8cf43f
Ioan Istrate <ioan1@Ioans-MBP.frontierlocal.net>
2018-10-26 14:16:24 -0400

    adds globals

---
+++ globals.h
@@ -0,0 +1,19 @@
+//
+//  globals.h
+//  warcraft
+//
+//  Created by Ioan Istrate on 10/26/18.
+//  Copyright © 2018 Ioan Istrate. All rights reserved.
+//
+
+#ifndef GLOBALS_H
+#define GLOBALS_H
+
+#define P(x) std::cout << #x << "'s value is " << x << std::endl;
+
+namespace GLOBALS {
+    const int SCREEN_WIDTH = 1200;
+    const int SCREEN_HEIGHT = 960;
+}
+
+#endif
